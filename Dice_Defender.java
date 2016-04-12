27db493e3c967c738e8f8754037dcbb112814aad
SariusRu <samuel.rundel@live.de>
2016-04-12 12:23:52 +0200

    Grundklasse Würfel
    Die Grundklasse für die spätere Würfelfunktion

---
+++ Dice_Defender.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Dice_Defender here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Dice_Defender extends Dice
+{
+    /**
+     * Act - do whatever the Dice_Defender wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }
+    /**
+     * @param Troups    Anzahl der Truppen auf dem gewählten Feld
+     * //@param cheat     optional:   Die Kleinstmöglichste Anzahl des Würfels, standardmäßig 1
+     * 
+     */
+    public int dice(int Troups)
+    {
+        int dice_eyes = 0;
+        return dice_eyes;
+    }
+}
