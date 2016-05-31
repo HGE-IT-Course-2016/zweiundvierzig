782e9dbb136b98c71efc7fe87628ec1d79e35b39
Achim <achim.retzbach@outlook.de>
2016-05-31 09:16:19 +0200

    Add files via upload

---
+++ ArmySchubser.java
@@ -0,0 +1,27 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.MouseInfo.*;
+
+/**
+ * Schubst Einheiten.
+ * 
+ * @author MaxiJohl, GruenerWal
+ * @version 0.0.1
+ */
+public class ArmySchubser extends Map_World
+{
+
+    /**
+     * Constructor for objects of class ArmySchubser.
+     * 
+     */
+    public ArmySchubser()
+    {
+        // Hi.
+    }
+    
+    public void act()
+    {
+        MouseInfo m = Greenfoot.getMouseInfo();
+        Actor startProvince = m.getActor();
+    }
+}
