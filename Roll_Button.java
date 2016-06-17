107e2256e1a135ef8cee37c6dcc36da5be65e2fa
Zocker1999NET <zocker.net@live.de>
2016-06-17 23:17:29 +0200

    Hinweise zu betroffenen Klassen hinzugefügt Die hier aufgeführten Klassen sollen mit Erreichen des Beta-Meilensteins aus dem Projekt entfernt werden. Die Begründungen sind in den jeweiligen Dateien direkt am Anfang zu entnehmen. Teilweise wird Code aus den Klassen in Andere transferiert. Sollte für Teile des Codes oder für den gesamten Code wieder eine Verwendung bestehen, wird die Entfernung wieder ausgesetzt.

--- Roll_Button.java
+++ Roll_Button.java
@@ -1,3 +1,9 @@
+/*
+    Hinweis zum Verbleib der Klasse:
+    Diese Klasse soll, da sie zurzeit weder vollständig, noch in Verwendung, noch in der Architektur definiert ist, zum Beta-Meilenstein entfernt werden.
+    Sollte die Klasse jedoch zuvor noch Verwendung finden, wird sie nach Absprache mit den jeweiligen Autoren/Benutzern in die Architektur aufgenommen.
+*/
+
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
