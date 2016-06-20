ceb3dc9348d122585dfc46070075bddd93cc90f8
Zocker1999NET <zocker.net@live.de>
2016-06-20 21:47:42 +0200

    Debug Teil 2 - Entfernen von kleinen Syntax Fehlern
    - *Colors*: Statt mit **color** wurde mit **color[]** versucht, das
    Array als Argument zu übergeben
    - *Start_Button*: Die beiden Werte der Variablen **text** und
    **textSize** wurden in den Konstruktoraufruf eingegliedert.
    Außerdem wurde der Einzug von 4 Leerzeichen auf Tabs umgestellt, was
    eine kleinere Dateigröße bedeutet.

--- Start_Button.java
+++ Start_Button.java
@@ -8,18 +8,19 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Start_Button extends Button
 {
-    private int textSize = 32; 
-	private String text = "New Game";
-    
-    /**
-     * Erstellt neue Klasse Colors.
-     */
-    public void act() 
-    {
-        setLocation(100,100);
-        if (Greenfoot.mouseClicked(this))    
-        {    
-            Greenfoot.setWorld(new Colors());  
-        }
-    }    
+	public Start_Button() {
+		super("New Game", 32);
+	}
+	
+	/**
+	 * Erstellt neue Klasse Colors.
+	 */
+	public void act() 
+	{
+		setLocation(100,100);
+		if (Greenfoot.mouseClicked(this))    
+		{    
+			Greenfoot.setWorld(new Colors());  
+		}
+	}    
 }
