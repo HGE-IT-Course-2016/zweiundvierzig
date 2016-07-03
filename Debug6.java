979e2e3e1a4605b2a111d51dc54fc70fb543588d
Zocker1999NET <zocker.net@live.de>
2016-07-03 21:57:50 +0200

    Syntax-Fehler verbessert

--- Debug6.java
+++ Debug6.java
@@ -1,5 +1,5 @@
-public class Debug5 extends Map_World {
-    public Debug5() {
+public class Debug6 extends Map_World {
+    public Debug6() {
         super(
             new String[] {"Spieler Nr. 1", "Player No. 2", "Spieler Nr. 3", "Player No. 4", "Spieler Nr. 5", "Player No. 6"},
             new int[] {1,2,3,4,5,6}
