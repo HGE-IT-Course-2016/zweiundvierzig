944ce13b1d127c39dd76a15b1ab712b4d493f161
4ngelica <an.knittel@gmail.com>
2016-07-04 23:26:58 +0200

    Aufrufen der Spielanleitung übers Menü

---
+++ Spielanleitung.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Spielanleitung here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Spielanleitung extends Actor 
+{
+    public void act() 
+    {
+        
+        getImage();
+        
+    }    
+    
+    
+}
